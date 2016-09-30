4faed40a4efcde9d7ec24d37022ae5c1e2316d60
katya20 <kpanchenko21@gmail.com>
2016-09-30 08:51:20 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 addMessage("LOL");
+
+var quantityInput = $(".element-quantity-input")
+
+
+quantityInput.keydown(function(e) {
+  var keyCode = e.keyCode;
+  var inputValue = quantityInput.val();
+  console.log ("hi");
+  // enter key!
+  if (keyCode == 13) {	
+    console.log("enter");
+    addElementsToThePage(inputValue);  
+  }
+})
