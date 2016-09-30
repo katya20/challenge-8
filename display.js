63593fc0a8db5d2003a80a1e02c55e9aaff328de
katya20 <kpanchenko21@gmail.com>
2016-09-30 08:54:23 -0700

    Classadoo autocommit

--- display.js
+++ display.js
@@ -7,16 +7,17 @@ listenForClear(function() {
   $(".message-display").html("")
 })
 
-console.log("hi");
+console.log("hello")
+
 var quantityInput = $(".element-quantity-input")
 
 quantityInput.keydown(function(e) {
   var keyCode = e.keyCode;
   var inputValue = quantityInput.val();
-  console.log ("hi");
+  console.log ("hi")
   // enter key!
   if (keyCode == 13) {	
-    console.log("enter");
+    console.log("enter")
     addElementsToThePage(inputValue);  
   }
 })
